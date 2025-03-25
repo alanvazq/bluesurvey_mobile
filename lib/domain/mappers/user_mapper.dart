b1c8b2bb3a646f101d0c6e28bb9176857618c5c5
Alan Gomez <alangmvaz@gmail.com>
2025-03-24 22:57:40 -0600

    refactor: cambio en usuario

--- lib/domain/mappers/user_mapper.dart
+++ lib/domain/mappers/user_mapper.dart
@@ -1,14 +1,11 @@
-import 'package:bluesurvey_app/domain/entities/role.dart';
 import 'package:bluesurvey_app/domain/entities/user.dart';
-import 'package:bluesurvey_app/domain/mappers/role_mapper.dart';
 
 class UserMapper {
   static User userJsonToEntity(Map<String, dynamic> json) => User(
-        id: json['id'],
-        email: json['email'],
-        username: json['username'],
-        roles:
-            List<Role>.from(json["roles"].map((x) => RoleMapper.fromJson(x))),
-        token: json['token'],
+        id: json['user']['id'],
+        email: json['user']['email'],
+        name: json['user']['name'],
+        accessToken: json['accessToken'],
+        refreshToken: json['refreshToken']
       );
 }
